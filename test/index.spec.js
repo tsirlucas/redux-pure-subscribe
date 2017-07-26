4630eed4a98c040879c1a71fdcd415bcd5403c2e
lucascorreia <tsirlucas@gmail.com>
2017-07-26 18:42:51 -0300

    Adding test cases

---
+++ test/index.spec.js
@@ -0,0 +1,17 @@
+describe('pureSubscribe function', () => {
+  it('should trigger function when someting changes', () => {
+
+  });
+
+  it('should not trigger function when nothing changes', () => {
+
+  });
+
+  it('should accept paths that you want to observe', () => {
+
+  });
+
+  it('should use all paths when specific path is not defined', () => {
+
+  });
+});
